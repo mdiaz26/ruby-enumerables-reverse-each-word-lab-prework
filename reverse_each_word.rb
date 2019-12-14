a45dd99eb55ae34477cd4f0dfc4821cd7ea1a140
Michael Diaz <mike.diaz006@gmail.com>
2019-12-14 00:29:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,5 @@ def reverse_each_word(string)
   array = string.split
   reverse_array = array.each { |n| n.reverse }
   new_string = reverse_array.join(" ")
-  new_string
-  puts array.each {|n| n.reverse }
+  reverse_array
 end
